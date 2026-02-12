d4f22d43d8e84aa60b9d385d75e18a7f60249e34
Michael Manuel <manuelmichael17@gmail.com>
2026-02-12 11:49:42 +0800

    fix CSP for cloudinary upload

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,35 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:', 'blob:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
+  {
+    name: 'strapi::body',
+    config: {
+      formidable: {
+        maxFileSize: 50 * 1024 * 1024, // 50MB
+      },
+    },
+  },
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
